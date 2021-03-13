fa3fcace71f0c5d708f25b1e813dc86cec0ed032
indonswe <afc2002afc@hotmail.com>
2021-03-13 16:56:33 +0100

    Initial commit

---
+++ src/main/java/com/example/jda/JdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jda/JdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
