3b20fe9d1bdbaa3e235a8590cada4aca18bb7a06
indonswe <afc2002afc@hotmail.com>
2021-03-13 17:06:13 +0100

    Created appuser in jda database

---
+++ src/main/java/com/example/jda/models/AppUser.java
@@ -0,0 +1,101 @@
+package com.example.jda.models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private int appUserId;
+    //@Column(unique = true)
+    private String username;
+    private String password;
+    private LocalDate regDate;
+    //@OneToOne(cascade = CascadeType.ALL)
+    //@JoinColumn(name = "details_id", table = "app_user")
+    //private Details userDetails;
+    //private String role;
+
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "appUserId=" + appUserId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", regDate=" + regDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return appUserId == appUser.appUserId &&
+                Objects.equals(username, appUser.username) &&
+                Objects.equals(password, appUser.password) &&
+                Objects.equals(regDate, appUser.regDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appUserId, username, password, regDate);
+    }
+
+    //public AppUser(int appUserId, String username, String password, LocalDate regDate, Details userDetails) {
+    public AppUser(int appUserId, String username, String password, LocalDate regDate) {
+        this.appUserId = appUserId;
+        this.username = username;
+        this.password = password;
+        this.regDate = regDate;
+        //this.userDetails = userDetails;
+    }
+
+
+    public AppUser() {
+    }
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    //public Details getUserDetails() {
+    //    return userDetails;
+    //}
+
+    //public void setUserDetails(Details userDetails) {
+        //this.userDetails = userDetails;
+    //}
+}
