07dca9e98f8bc49b274daf495525d75d720096e5
indonswe <afc2002afc@hotmail.com>
2021-03-14 11:47:07 +0100

    Work on findbyid in AppUserDaoRepository

--- src/main/java/com/example/jda/Data/AppUserDaoRepository.java
+++ src/main/java/com/example/jda/Data/AppUserDaoRepository.java
@@ -1,12 +1,28 @@
 package com.example.jda.Data;
 
 import com.example.jda.models.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
+import javax.persistence.EntityManager;
 import javax.xml.crypto.Data;
 import java.util.Collection;
 
 public class AppUserDaoRepository implements com.example.jda.Data.AppUserDAO {
+
+   private final EntityManager em;
+
+    @Autowired
+    public AppUserDaoRepository(EntityManager em) {
+        this.em = em;
+    }
+
+
+    /*@Override
+    @Transactional(readOnly = true)
+    public Details findById(Integer integer) {
+        return em.find(Details.class, integer);
+    }*/
 
 
     @Override
@@ -16,7 +32,7 @@ public class AppUserDaoRepository implements com.example.jda.Data.AppUserDAO {
 
     @Override
     public AppUser findById(Integer appUserID) {
-        return null;
+        return em.find(AppUser.class,appUserID);
     }
 
     @Override
