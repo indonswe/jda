1f854fa3c13ac79ef571fc475327692f58951690
indonswe <afc2002afc@hotmail.com>
2021-03-17 12:19:16 +0100

    Starting on bookDAORepository

--- src/main/java/com/example/jda/Data/BookDAORepository.java
+++ src/main/java/com/example/jda/Data/BookDAORepository.java
@@ -1,5 +1,34 @@
 package com.example.jda.Data;
 
-public class BookDAORepository {
+import com.example.jda.models.Book;
 
+import java.util.Collection;
+
+public class BookDAORepository implements BookDAO{
+
+
+    @Override
+    public Book findById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Collection<Book> findAll() {
+        return null;
+    }
+
+    @Override
+    public Book create(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer bookId) {
+
+    }
 }
